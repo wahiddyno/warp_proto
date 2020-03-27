fb1886e688d49c53023c6c4c4400e574b4e777ae
Abdul Wahid I <34985757+wahiddyno@users.noreply.github.com>
2020-03-27 09:41:23 +0530

    second level commit

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 import boto3
-from config import S3_BUCKET, S3_KEY, S3_SECRET
+from itsdangerous import URLSafeSerializer
+from config import S3_BUCKET, S3_KEY, S3_SECRET, INDEX, link_prefix
 from flask import session
 
 
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = 'warpit'
+auth_s = URLSafeSerializer(app.secret_key, "auth")
 
 
 def _get_s3_resource():
@@ -36,9 +38,27 @@ def get_buckets_list():
     return client.list_buckets().get('Buckets')
 
 
+def put_object(obj):
+    bucket_obj = get_bucket()
+    file_name = obj.filename
+    token = auth_s.dumps(file_name)
+    bucket_obj.upload_fileobj(obj, token, ExtraArgs={
+        "ACL": "public-read",
+        "ContentType": obj.content_type
+    })
+    return(token)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    current_file = request.files['file']
+    key = put_object(obj=current_file)
+    return render_template('successful.html', key=link_prefix + str(key))
 
 
 @app.route('/files')
--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_KEY = os.environ.get("S3_KEY")
 S3_SECRET = os.environ.get("S3_SECRET")
+INDEX = os.environ.get("INDEX")
+link_prefix = "https://warp-proto.s3.ap-south-1.amazonaws.com/"
